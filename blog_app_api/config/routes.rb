908577093dd7ba021cca1ab6d4ac12eeef18396f
jimmikmak <73326512+jimmikmak@users.noreply.github.com>
2021-03-20 16:12:06 +1100

    Routes set-up

--- blog_app_api/config/routes.rb
+++ blog_app_api/config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  get '/welcome', to: 'welcome#index'
-  resources :todos
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
