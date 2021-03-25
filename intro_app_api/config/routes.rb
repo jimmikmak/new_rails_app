9afa01d68b0f5aa8000ed7027201bb2916e9c14f
jimmikmak <73326512+jimmikmak@users.noreply.github.com>
2021-03-25 19:04:33 +1100

    new api file

--- intro_app_api/config/routes.rb
+++ intro_app_api/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/welcome', to: 'welcome#index'
-  resources :todos
+  namespace :api do
+    resources :todos
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
