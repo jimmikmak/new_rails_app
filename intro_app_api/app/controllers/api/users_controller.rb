e93a8e79411b24dcc8e00eb96ead672d4066c384
jimmikmak <73326512+jimmikmak@users.noreply.github.com>
2021-03-27 09:53:45 +1100

    auth_controller

--- intro_app_api/app/controllers/api/users_controller.rb
+++ intro_app_api/app/controllers/api/users_controller.rb
@@ -5,18 +5,14 @@ class Api::UsersController < ApplicationController
     render json: User.all
   end
 
-  #   def create
-  #     # puts params[:title]
-  #     # puts params[:completed]
-  #     # puts params[:description]
-  #     todo = Todo.create(title: params[:title], completed: params[:completed], description: params[:description])
-  #     todo_valid = todo.valid?
-  #     if todo_valid
-  #       render json: { message: 'Successfully created todo list!' }, status: 200
-  #     else
-  #       render json: { message: 'Unable to create todo list' }, status: 400
-  #     end
-  #   end
+  def create
+    user = User.create(user_params)
+    if user.valid?
+      render json: user, status: 201
+    else
+      render json: { message: 'Unable to create user!' }, status: 500
+    end
+  end
 
   # Todo.create(title: "James", completed: false, description:)
 
@@ -30,6 +26,10 @@ class Api::UsersController < ApplicationController
   #     todo.update(completed: params[:completed], description: params[:description], title: params[:title])
   #     render json: { type: 'Successfully updated entry!' }
   #   end
+
+  def update
+    render json: User.find(params[:id]).update(user_params)
+  end
 
   def destroy
     User.destroy(params[:id])
--- intro_app_api/app/controllers/application_controller.rb
+++ intro_app_api/app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  def encode_token(user)
+    JWT.encode(user, 'super-secret-password')
+  end
+
+  def decode_token; end
 end
