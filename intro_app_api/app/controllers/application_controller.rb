d81d00860509b3b944b1241553e8c6d8124ba0f7
jimmikmak <73326512+jimmikmak@users.noreply.github.com>
2021-03-27 13:34:18 +1100

    front-end added

--- intro_app_api/app/controllers/application_controller.rb
+++ intro_app_api/app/controllers/application_controller.rb
@@ -1,9 +1,21 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  # DON'T hardcode key argument ie 'super-secret*&^%' as this will be in source control or git.
+  # Store in a variable (system variable) outside the code.
   def encode_token(user_id)
     JWT.encode(user_id, 'super-secret*&^%')
   end
 
-  def decode_token; end
+  def decode_token
+    auth_token = request_headers['Authorization']
+    token = auth_token.split[' '] if auth_token
+    p 'TOKEN'
+    p token
+    begin
+      JWT.decode token[1], 'super-secret*&^%'
+    rescue StandardError
+      nil
+    end
+  end
 end
