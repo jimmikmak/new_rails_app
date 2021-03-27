60f66b7065eec2cbd52afb86be5c4b3fc211ab8c
jimmikmak <73326512+jimmikmak@users.noreply.github.com>
2021-03-27 11:47:53 +1100

    token generated

--- intro_app_api/app/controllers/api/auth_controller.rb
+++ intro_app_api/app/controllers/api/auth_controller.rb
@@ -1,1 +1,13 @@
 # frozen_string_literal: true
+
+class Api::AuthController < ApplicationController
+  def login
+    user = User.find_by(name: params[:name])
+    if user&.authenticate(params[:password])
+      token = encode_token(user.id)
+      render json: { user: user, token: token }
+    else
+      render json: { message: 'user not found' }, status: :not_found
+    end
+  end
+end
--- intro_app_api/app/controllers/application_controller.rb
+++ intro_app_api/app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::API
-  def encode_token(user)
-    JWT.encode(user, 'super-secret-password')
+  def encode_token(user_id)
+    JWT.encode(user_id, 'super-secret*&^%')
   end
 
   def decode_token; end
